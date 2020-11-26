31ff1014b831dee223b579d984891d5b2a6c19b6
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-26 16:09:39 +0200

    add? del? up

--- src/br_3/AddNumbers.java
+++ src/br_3/AddNumbers.java
@@ -4,17 +4,18 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5, int num2 = 15, int sum;
+      int num1 = 7;
+	  int num2 = 17;
+	  int sum;
 
       sum = num1 + num2;
       
       logger.log("Sum of these numbers: " + sum);
    }
    
-   public static void main(String[] args) {
+   public static void main(String args[]) {
       
-      int num1 = 3;
-	  int num2 = 14;
+      int num1 = 3, num2 = 14;
 	  int dif;
 
       del = num1 - num2;
@@ -22,14 +23,4 @@ public class AddTwoNumbers {
       logger.log("Dif of these numbers: " + dif);
    }
 
-   public static void main(String[] args) {
-      
-      int num1 = 5;
-	  int num2 = 15;
-	  int del;
-
-      del = num1 / num2;
-	  
-      logger.log("Sum of these numbers: " + del);
-   }
 }
