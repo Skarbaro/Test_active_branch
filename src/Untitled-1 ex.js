f8cdd3df6000162b41b37faddea7f0aad837b6c5
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-27 20:27:43 +0200

    asd master 1 file test 2

---
+++ src/Untitled-1 ex.js
@@ -0,0 +1,7 @@
+const invUser_selector = ':nth-child(1) > .col-user > .cq-avatar > .cq-avatar-username > .cq-avatar-username-login';
+const userOptions_xpath = 'img[src$="/images/menu-options-dark.svg"]';
+const deleteInvUser_selector = '.dropdown-menu > :nth-child(2) > span';
+
+const select_projects = '#projects > .css-vj8t7z > .css-1hwfws3';
+const permissions_selector = '#permissions > .css-vj8t7z > .css-1hwfws3';
+const back_button_withInviteForm = '.pl-0 > .btn';
