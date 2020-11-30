7f6de92e3eb6967b4b4dabd45172e0cec49fae7d
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-30 13:45:50 +0200

    add file, up

--- src/br_3/TwoNumb.java
+++ src/br_3/TwoNumb.java
@@ -4,9 +4,7 @@ public class TwoNumb {
 
    public static void main(String[] args) {
       
-      int num1 = 5;
-	  int num2 = 15;
-	  int del;
+      int num1 = 5, int num2 = 15, int del;
 
       del = num1 / num2;
 	  
