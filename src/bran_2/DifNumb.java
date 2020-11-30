31938d2685ef47f757e4f5222bea647f4fe1ca6b
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-30 16:55:40 +0200

    del. up 11213

--- src/bran_2/DifNumb.java
+++ src/bran_2/DifNumb.java
@@ -1,12 +1,15 @@
+package src.java;
+
 public class DifNumbers {
 
    public static void main(String[] args) {
-      int num1 = 5, num2 = 15, dif;
+      int num1 = 5;
+	  int num2 = 15, dif;
       dif = num1 - num2;
       System.out.println("Sum of these numbers: " + dif);
    }
    
-   public static void main(String[] args) {
+   public static void main(String args[]) {
       int red1 = 5,
 	  int red2 = 17, rif;
       rif = red1 - red2;
