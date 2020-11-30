468bd0241e9a0bf4a01aaf03afde6302a8605e39
Alex Romaniuk <alaster.rom@gmail.com>
2020-11-30 18:21:03 +0200

    fix

--- src/bran_2/DifNumb.java
+++ src/bran_2/DifNumb.java
@@ -8,9 +8,10 @@ public class DifNumbers {
       System.out.println("Sum of these numbers: " + dif);
    }
    
-   public static void main(String[] args) {
-      int red1 = 5,
-	  int red2 = 17, rif;
+   public static void main(String args[]) {
+	   
+	   private int tel;
+      int red1 = 5, red2 = 10, rif;
       rif = red1 - red2;
       System.out.println("Sum of these numbers: " + rif);
    }
