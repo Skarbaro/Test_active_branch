8d98febf89e53fc24f02c707f9de1c062c8fedaa
Alex Raf <raf.afm@gmail.com>
2020-12-01 17:11:39 +0200

    add file inof

---
+++ src/DifNumb.java
@@ -0,0 +1,23 @@
+package src.java;
+
+public class DifNumbers {
+	
+	private int deo;
+
+   public static void main(String args[]) {
+      int num1 = 5, num2 = 15, dif;
+	  int def;
+      dif = num1 - num2;
+      System.out.println("Sum of these numbers: " + dif);
+	  System.out.println("Sum of these numbers: " + def);
+   }
+   
+   public static void main(String args[]) {
+	   
+	   private int tel, get, red1 = 5, red2 = 10, rif;
+      rif = red1 - red2;
+      System.out.println("Sum of these numbers: " + rif);
+   }
+  
+
+}
