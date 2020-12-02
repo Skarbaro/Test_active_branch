05cd50ac8fdb67ca7925d498c4f6c857cb093cd4
Miracle <miravle.afm@gmail.com>
2020-12-02 10:36:35 +0200

    add file code and up exemple code java/

--- src/bran_2/DifNumb.java
+++ src/bran_2/DifNumb.java
@@ -11,10 +11,46 @@ public class DifNumbers {
    public static void main(String args[]) {
 	   
 	   private int tel;
-      int red1 = 5, red2 = 10, rif;
+      int red1 = 5; 
+	  int red2 = 10, rif;
       rif = red1 - red2;
       System.out.println("Sum of these numbers: " + rif);
+
    }
-  
+   
+   
+  public static void main(String[] args) {
+
+    // year to be checked
+    int year = 1996;
+    boolean leap = false;
+
+    // if the year is divided by 4
+    if (year % 4 == 0) {
+
+      // if the year is century
+      if (year % 100 == 0) {
+
+        // if year is divided by 400
+        // then it is a leap year
+        if (year % 400 == 0)
+          leap = true;
+        else
+          leap = false;
+      }
+      
+      // if the year is not century
+      else
+        leap = true;
+    }
+    
+    else
+      leap = false;
+
+    if (leap)
+      System.out.println(year + " is a leap year.");
+    else
+      System.out.println(year + " is not a leap year.");
+  }
 
 }
